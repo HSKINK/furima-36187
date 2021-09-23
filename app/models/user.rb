3308609f4416a984ea59726c5730a957c0f51efe
HSKINK <yuhikwut@gmail.com>
2021-09-23 19:10:17 +0900

    ビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
-            :last_name,       presence: true
-            :first_name,      presence: true
-            :last_name_kana,  presence: true
-            :first_name_kana, presence: true
-            :birthday,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday,        presence: true
 end
