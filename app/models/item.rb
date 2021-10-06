aed4edca0069e1241b6e1cb7d4e7882659e55eec
HSKINK <yuhikwut@gmail.com>
2021-10-06 20:40:32 +0900

    出品データの登録

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,19 @@
 class Item < ApplicationRecord
  
-  validates :image, presence: true
-  validates :product, presence: true
+  validates :image,       presence: true
+  validates :product,     presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :content_id, presence: true
-  validates :charge_id, presence: true
-  validates :area_id, presence: true
-  validates :ship_id, presence: true
-  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :content_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :charge_id,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id,     numericality: { other_than: 1 , message: "can't be blank"}
+  validates :ship_id,     numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,       numericality: {in: 300..9_999_999}, format: {with: /\A[0-9]+\z/ }
 
   has_one_attached :image
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :content, :charge, :area, :ship
+
 end
