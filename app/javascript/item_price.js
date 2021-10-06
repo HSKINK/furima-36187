5fbd9d0701074a514be48529b55b206b477731e5
HSKINK <yuhikwut@gmail.com>
2021-10-06 22:28:33 +0900

    販売利益の描画

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,11 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * 1.1)
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
+    const profit = document.getElementById("profit");
+    profit.innerHTML = inputValue - addTaxDom.innerHTML
   })
 });
