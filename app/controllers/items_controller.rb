f46db2dc763f4bf32e62055a69a595478aa86706
HSKINK <yuhikwut@gmail.com>
2021-10-14 21:40:33 +0900

    商品情報編集機能 修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :item_find_params, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order('id DESC')
@@ -19,16 +20,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -41,5 +39,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :product, :description, :category_id, :content_id, :charge_id, :area_id, :ship_id,
                                  :price).merge(user_id: current_user.id)
+  end
+
+  def item_find_params
+    @item = Item.find(params[:id])
   end
 end
